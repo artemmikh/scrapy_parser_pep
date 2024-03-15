483b0ee31e2f9a51ecdd116788ba51d2a016694a
artemmikh <artem.mikh100@gmail.com>
2024-03-15 13:36:57 +0300

    рабочий проект, не проходит один тест

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,31 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+import os
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from scrapy.utils.project import get_project_settings
+from pep_parse.settings import BASE_DIR
 
 
 class PepParsePipeline:
+    def __init__(self):
+        self.status_counts = {}
+        self.total_count = 0
+
+    def open_spider(self, spider):
+        # self.base_dir = get_project_settings().get('BASE_DIR')
+        self.base_dir = BASE_DIR
+
     def process_item(self, item, spider):
+        status = item['status']
+        self.status_counts.setdefault(status, 0)
+        self.status_counts[status] += 1
+        self.total_count += 1
         return item
+
+    def close_spider(self, spider):
+        filename = os.path.join(self.base_dir, '..', 'results', f'status_summary_{spider.time}.csv')
+        with open(filename, mode='w', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Статус', 'Количество'])
+            for status, count in self.status_counts.items():
+                writer.writerow([status, count])
+            writer.writerow(['Total', self.total_count])
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,32 @@
+from datetime import datetime
+
 import scrapy
+
+from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
+    time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     def parse(self, response):
-        pass
+        """Cобирает ссылки на документы PEP."""
+        table = response.css('section#numerical-index table')
+        tbody_tr = table.css('tbody tr')
+        # test
+        # for tr in tbody_tr[:5]:
+        for tr in tbody_tr:
+            link = tr.css('a.pep.reference.internal::attr(href)').get()
+            yield response.follow(link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        """Парсит страницы с документами и формирует Items."""
+        number_name = response.css('h1.page-title::text').get().split(' – ')
+        data = {
+            'number': number_name[0].split(' ')[1],
+            'name': number_name[1],
+            'status': response.css('abbr::text').get()
+        }
+        yield PepParseItem(data)
